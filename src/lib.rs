39b6af45f985e1ad97c1ed363bc3580c4d34eec7
nanocryk <6422796+nanocryk@users.noreply.github.com>
2024-02-17 01:41:15 +0100

    assert_fields_eq!

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,11 @@ mod anon;
 mod clone;
 mod spread;
 
+// public to re-export `assert_eq!` from either `core` or `similar_asserts` based on the
+// `similar-asserts` feature.
+#[doc(hidden)]
+pub mod assert_fields_eq;
+
 /// Standalone [`Default::default()`] function.
 pub fn default<T: Default>() -> T {
     T::default()
